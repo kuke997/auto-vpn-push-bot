b7dc8193c30dd155a0768bfe64d12b3f52b8cc65
kuke997 <ttnf918@gmail.com>
2025-06-01 11:31:25 -0400

    main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 CHANNEL_ID = os.getenv('CHANNEL_ID')
 
 # 示例 Clash 节点订阅链接（YAML 格式）
-SUBSCRIBE_URL = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/aiboboxx/clashfree/main/clash.yml"
+SUBSCRIBE_URL = "https://node.freeclashnode.com/uploads/2025/06/0-20250601.yaml"
 
 def get_nodes():
     try:
